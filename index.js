94c8fb50f1e6b3f20cc961bf7ee36cda7ae7e1b9
gersonaltamirano <gersonaltamirano@gmail.com>
2024-09-18 17:27:43 -0600

    servidor configurado

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ import ffmpeg from "ffmpeg-static";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
+import dotenv from "dotenv"
+
+const {
+    PORT_HTTP,
+    PORT_RTMP,
+    PORT_VIDEO,
+    URL_BASE,
+    URL_DOMINIO,
+    URL_RTMP,
+    URL_VIDEO,
+} = dotenv.config().parsed
+
 
 // Simular __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -21,17 +33,17 @@ if (!fs.existsSync(mediaRootPath)) {
 // Configuración dinámica
 const config = {
   rtmp: {
-    port: 1935,
+    port: PORT_RTMP,
     chunk_size: 60000,
     gop_cache: true,
-    ping: 10,
-    ping_timeout: 30,
+    ping: 30,
+    ping_timeout: 60,
   },
   http: {
-    port: 8000,
+    port: PORT_VIDEO,
     allow_origin: "*",
     // api: true,
-	mediaroot: "./media",
+    mediaroot: "./media",
   },
   trans: {
     ffmpeg: ffmpegPath,
@@ -45,8 +57,23 @@ const nms = new NodeMediaServer(config);
 // Express para manejar las rutas dinámicas
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Reproductor de canales en vivos</title>
+    </head>
+    <body>
+      <h1>Para reproducir un canal en vivo coloque la ruta correcta</h1>
+    </body>
+    </html>
+  `);
+});
+
 // Crear rutas dinámicas para canales
-app.get("/:channel", (req, res) => {
+app.get("/channel/:channel", (req, res) => {
+  console.log("Solicitud de canal", req.params.channel);
   const channelName = req.params.channel;
 
   // Crear un directorio para almacenar los archivos HLS/DASH de cada canal
@@ -66,6 +93,9 @@ app.get("/:channel", (req, res) => {
     mediaRoot: channelPath, // Directorio específico para el canal
   });
 
+  const url_m3u8 = "https://" + URL_BASE + "-" + URL_VIDEO + "." + URL_DOMINIO + "/" + channelName + "/index.m3u8"
+
+
   // Responder al cliente con información del canal
   //   http://localhost:8000/${channelName}/index.m3u8
   res.send(`
@@ -80,6 +110,10 @@ app.get("/:channel", (req, res) => {
   href="https://vjs.zencdn.net/7.2.3/video-js.css"
   rel="stylesheet" />
 
+  <a href="${url_m3u8}">lista</a>
+  <h1>URL PARA TRANSMITIR EN VIVO: </h1>
+  <h1>rtmp://${URL_BASE}-${URL_RTMP}.${URL_DOMINIO}/${channelName}</h1>
+
 	<!-- HTML -->
 	<video
 	id="hls-example"
@@ -89,7 +123,7 @@ app.get("/:channel", (req, res) => {
 	controls>
 	<source
 		type="application/x-mpegURL"
-		src="http://localhost:8000/${channelName}/index.m3u8" />
+		src="${url_m3u8}" />
 	</video>
 	<!-- JS code -->
 	<!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
@@ -106,8 +140,8 @@ app.get("/:channel", (req, res) => {
 });
 
 // Iniciar el servidor de express en el puerto 8001
-app.listen(8001, () => {
-  console.log("Servidor HTTP dinámico corriendo en puerto 8001");
+app.listen(PORT_HTTP, () => {
+  console.log("Servidor HTTP dinámico corriendo en puerto " + PORT_HTTP);
 });
 
 // Iniciar el servidor de transmisión
